eb82bdedb15bb82e28c6e9513e02c4c3a97c8d73
githubalvin <370736605@qq.com>
2021-04-16 10:39:16 +0800

    android support

--- 安卓-自动刷痴.bat
+++ 安卓-自动刷痴.bat
@@ -8,8 +8,8 @@ PUSHD %~DP0 & cd /d "%~dp0"
 mshta vbscript:createobject("shell.application").shellexecute("%~s0","goto :runas","","runas",1)(window.close)&goto :eof
 :runas
  
-::填写自己的脚本
-python main.py  --android 34104c43 auto 1
+::填写自己的设备号（--android后）
+python main.py --android 34104c43 auto 1
  
 echo 执行完毕,任意键退出
  
