ab34a55bb7556de2be3bcc9f6c05c030e83465db
githubalvin <370736605@qq.com>
2021-04-21 14:43:35 +0800

    fixbug

--- script/yuling.py
+++ script/yuling.py
@@ -17,6 +17,7 @@ step1 = image("yeyuanhuo_chi_challenge.png", (1.059, 0.296), (1152, 679))
 def challenge():
     touch(wait(step1, timeout=60)) 
     pos = wait(GLOBAL_SIFT_CONTINUE, timeout = 120)
+    touch_pos(random_pos(pos, 50, 100, 0, 1), 2)
     time.sleep(4)
     touch_pos(random_pos(pos, 50, 100, 0, 1), 2)
 
